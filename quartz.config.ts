a57fc5e26e7a897ce1c184e66d8b02c6a296509d
Elambharathi Thangavel <elambharathi2002.pt@gmail.com>
2026-02-17 17:42:15 +0530

    Quartz sync: Feb 17, 2026, 5:42 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "elampt.github.io",
+    baseUrl: "elambharathi.com",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
@@ -86,6 +86,7 @@ const config: QuartzConfig = {
       }),
       Plugin.Assets(),
       Plugin.Static(),
+      Plugin.CNAME(),
       Plugin.NotFoundPage(),
       // Comment out CustomOgImages to speed up build time
       Plugin.CustomOgImages(),
